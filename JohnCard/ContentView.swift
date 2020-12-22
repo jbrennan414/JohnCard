4fe09208238e94c7c2eb5cfa1e4e57feb22dcae6
John Brennan <jbrennan@fluentstream.com>
2020-12-21 18:19:34 -0700

    Initial Commit

---
+++ JohnCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JohnCard
+//
+//  Created by John Brennan on 12/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JohnCard/JohnCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JohnCardApp.swift
+//  JohnCard
+//
+//  Created by John Brennan on 12/21/20.
+//
+
+import SwiftUI
+
+@main
+struct JohnCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
